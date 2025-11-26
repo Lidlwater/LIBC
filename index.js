0025a61ce0ff863546fb982d9122045150982590
Lidlwater <busallan88@gmail.com>
2025-11-26 16:02:27 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'monospace' }}>
-      <h1>🚀 LidlWater</h1>
-      <p>AI-scanner frontend online!</p>
-    </main>
-  );
-}
