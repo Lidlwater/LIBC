61a7cc71fee582003295321f419506811c2e86de
Lidlwater <busallan88@gmail.com>
2025-11-26 15:40:32 +0100

    Create settings.js

---
+++ pages/pages/pages/settings.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+
+export default function Settings() {
+return (
+<div className="p-6">
+<Navbar />
+<h1 className="text-2xl mb-4">⚙️ Inställningar</h1>
+<p>Här kommer användarinställningar.</p>
+</div>
+);
+}
