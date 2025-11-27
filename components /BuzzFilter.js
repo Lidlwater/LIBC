27bd458582bc333f02c0014ff4232ac9dc277169
Lidlwater <busallan88@gmail.com>
2025-11-27 09:06:56 +0100

    Delete components  directory

--- components /BuzzFilter.js
+++
@@ -1,9 +0,0 @@
-export default function BuzzFilter({ value, onChange }) {
-  return (
-    <div className="space-x-2 my-2">
-      <button onClick={() => onChange("low")} className={value === "low" ? "underline" : ""}>Låg</button>
-      <button onClick={() => onChange("medium")} className={value === "medium" ? "underline" : ""}>Medel</button>
-      <button onClick={() => onChange("high")} className={value === "high" ? "underline" : ""}>Hög</button>
-    </div>
-  );
-}
--- components /Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="p-4 bg-black text-white font-bold text-xl shadow-md">
-      🚀 LidlWater Internet Business Company
-    </header>
-  );
-}
